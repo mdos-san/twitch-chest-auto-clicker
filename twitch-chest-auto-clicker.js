f921d8a4033d31bdb79785259d34121ae10c9b5a
mdos-san <mdossan@protonmail.com>
2025-05-31 07:04:42 +0200

    update path of svg

--- twitch-chest-auto-clicker.js
+++ twitch-chest-auto-clicker.js
@@ -1,15 +1,15 @@
 // Twitch auto chest clicker
 
 function twitchChestAutoClicker() {
-	// Query the chest with the path attribute
-  var path = document.querySelector("path[d=\"M16.503 3.257L18 7v11H2V7l1.497-3.743A2 2 0 015.354 2h9.292a2 2 0 011.857 1.257zM5.354 4h9.292l1.2 3H4.154l1.2-3zM4 9v7h12V9h-3v4H7V9H4zm7 0v2H9V9h2z\"]");
+  // Query the chest with the path attribute
+  var path = document.querySelector("path[d=\"M2 6v11h16V6a3 3 0 0 0-3-3H5a3 3 0 0 0-3 3Zm10-1H8v2h4V5Zm2 0v2h2V6a1 1 0 0 0-1-1h-1ZM4 9v6h12V9H4Zm1-4h1v2H4V6a1 1 0 0 1 1-1Z\"]");
 
-	// If chest is present in dom, click on it
+  // If chest is present in dom, click on it
   if (path) {
     path.ownerSVGElement.parentNode.click()
   }
 
-	// Run this function once every 3 seconds
+  // Run this function once every 3 seconds
   window.setTimeout(twitchChestAutoClicker, 3 * 1000);
 }
 
